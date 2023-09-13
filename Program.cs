196da73175ecf83835973ae8d66e9fa0e0b91631
Duttatreya <Duttatreya@RISHI-WINDOWS>
2023-09-14 00:13:47 +0530

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿using leetcode.arrays_and_hashing;
+var obj = new EncodeDecodeStrings();
+
+List<string> strings = new List<string>{ "leet", "code", "loves", "you" };
+
+Console.WriteLine(obj.encode(strings)); 
+foreach(string s in obj.decode(obj.encode(strings))) {
+    Console.WriteLine(s.To);
+}
+string
---
+++ playground/Extension.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace csharp_playground.playground
+{
+    public static class Extension
+    {
+        public static List<T> Filter<T>(this List<T> records, Func<T, bool> func)
+        {
+            List<T> result = new List<T>();
+            foreach(T record in records)
+            {
+                if (func(record))
+                {
+                    result.Add(record);
+                }
+            }
+
+            return result;
+        }
+
+
+        public static string CapitalizeSentence(this string str)
+        {
+            if(str == null)
+            {
+                throw new ArgumentNullException(nameof(str));
+            }
+            string res = "";
+            string[] tokens = str.Split(" ");
+            foreach(string token in tokens)
+            {
+                res += char.ToUpper(token[0]) + token.Substring(1) + " ";
+            }
+            return res;
+        }
+    }
+}
